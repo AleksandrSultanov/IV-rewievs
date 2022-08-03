e16a75dc360b7e6158206a8fe8da7dc946511221
Aleksandr Sultanov <alexul603@gmail.com>
2022-08-03 10:29:57 +0300

    Исправил namespace, оформление кода хранилища.

--- scr/store/Reviews.php
+++ scr/store/Reviews.php
@@ -3,18 +3,18 @@
 namespace Intervolga\Reviews\store;
 include_once dirname(__DIR__)."/vendor/autoload.php";
 
-use Intervolga\reviews\Review;
+use Intervolga\Reviews\Review;
 use SQLite3;
 
 class Reviews
 {
     public SQLite3 $connection;
-    function __construct(SQLite3 $connection){
+    function __construct(SQLite3 $connection) {
         $this->connection = $connection;
     }
 
-    function findById(int $id): ?Review{
-        $result = $this->connection->query('SELECT * FROM reviews WHERE id=' . '$id');
+    function findById(int $id): ?Review {
+        $result = $this->connection->query("SELECT * FROM reviews WHERE id="."$id");
         $reviewInfo = $result->fetchArray();
         return new Review(
             $reviewInfo["id"],
@@ -31,7 +31,7 @@ class Reviews
     function find(int $id): array
     {
         // Запрашиваем определенное количетсво записей
-        $result = $this->connection->query("SELECT * FROM `reviews` LIMIT $id,20");
+        $result = $this->connection->query('SELECT * FROM `reviews` LIMIT "$id",20');
         $reviewsInfo = $result->fetchArray();
         $reviews = array();
 
